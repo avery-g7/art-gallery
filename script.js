a47ee94d0df0e876234913ff79ca62b8e2e228d4
Avery <averygadd7@gmail.com>
2023-07-22 18:23:48 -0600

    added top button

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let topBtn = document.querySelector('.topBtn');
+
+
+//button that goes back to the top of the page when clicked
+window.onscroll = function() {
+    scrollFunction()
+    scroll()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    };
+};
+topBtn.addEventListener("click", toTop);
+
+function toTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
